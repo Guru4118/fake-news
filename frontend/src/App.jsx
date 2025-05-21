100f50e4879293e085e1dd41e226c5d3532c56d7
Guru4118 <165055820+Guru4118@users.noreply.github.com>
2025-05-22 03:15:35 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,9 +13,9 @@ export default function App() {
       <Route
         path="/dashboard"
         element={
-          <PrivateRoute>
+          
             <Dashboard />
-          </PrivateRoute>
+          
         }
       />
     </Routes>
