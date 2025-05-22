8cbd9ed061bd2e04f7bcdaec8b057d9e2f72020c
Guru4118 <165055820+Guru4118@users.noreply.github.com>
2025-05-22 17:06:44 +0530

    Update factCheckRoutes.js

--- backend/routes/factCheckRoutes.js
+++ backend/routes/factCheckRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const { upload } = require('../middleware/upload');
-const { protect } = require('../middleware/authMiddleware');
-const { uploadAndCheck } = require('../controller/factCheckController');
 const { optionalAuth } = require('../middleware/optionalAuth');
+const { uploadAndCheck } = require('../controller/factCheckController');
 
 const router = express.Router();
-router.post('/image', protect, upload.single('image'), uploadAndCheck);
+
 router.post('/image', optionalAuth, upload.single('image'), uploadAndCheck);
 
 module.exports = router;
